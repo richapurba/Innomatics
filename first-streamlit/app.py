2ba15604595d0ca3c03a388f9c31e35ce22c26cf
Richa Purba <61378336+richapurba@users.noreply.github.com>
2023-03-02 16:51:41 +0700

    Insert the page title, header, and subheader

--- first-streamlit/app.py
+++ first-streamlit/app.py
@@ -3,4 +3,6 @@ import streamlit as st
 import plotly.express as px
 from PIL import Image
 
-
+st.set_page_config(page_title="NASA Discovery")
+st.header("Habitable Exoplanet and Stars 2023")
+st.subheader("Are we already have a new home?")
