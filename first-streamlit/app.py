54eb192b2e7d368864cc8e9e360b66ff004b7bc4
Richa Purba <61378336+richapurba@users.noreply.github.com>
2023-03-02 16:59:07 +0700

    Load the dataset

--- first-streamlit/app.py
+++ first-streamlit/app.py
@@ -3,6 +3,16 @@ import streamlit as st
 import plotly.express as px
 from PIL import Image
 
+
+#Page title, header, and subheader
 st.set_page_config(page_title="NASA Discovery")
 st.header("Habitable Exoplanet and Stars 2023")
 st.subheader("Are we already have a new home?")
+
+#Load the dataset
+excel = "Habitable.csv"
+sheet_name = "DATA"
+
+df = pd.read_excel(excel, sheet_name=sheet_name)
+
+st.dataframe(df)
